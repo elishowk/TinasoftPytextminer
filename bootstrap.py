14c90f483f99b14223d611e292bc5f74aca8d3cb
Julian bilcke <julian.bilcke@iscpif.fr>
2009-11-24 10:45:58 +0100

    optimized dependencies

--- bootstrap.py
+++ bootstrap.py
@@ -6,8 +6,22 @@ try:
 except:
     arch = ""
 import sys
-sys.path = ["lib/python-%s.%s_%s%s" % (sys.version_info[0],sys.version_info[1], sys.platform, arch)  , 'src'] + sys.path
 
+sys.path = [ 
+   "lib/python-%s.%s_%s%s" % (sys.version_info[0],sys.version_info[1], sys.platform, arch), # C MODULES
+   "lib/python-%s.%s" % (sys.version_info[0],sys.version_info[1]), # PYTHON MODULES
+    'src'] + sys.path # PROJECT MODULES, AND DEFAULT SYSTEM MODULES 
+print sys.path
+
+try:
+    import nltk
+    import yaml
+    import jsonpickle
+    import pickle
+    import os
+    import sys
+except Exception, exc:
+    print "Dependency not found:", exc
 # import nltk data
 import nltk
 nltk.data.path = ['shared/nltk_data']
--- lib/python-2.6_linux2_x86_64/simplejson/tests/test_check_circular.py
+++
@@ -1,1 +0,0 @@
-/usr/share/pyshared/simplejson/tests/test_check_circular.py
