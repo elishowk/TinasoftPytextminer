a1adb540966593decd79ab8a67dafe50e7eebc8a
Julian bilcke <julian.bilcke@iscpif.fr>
2009-11-24 11:12:37 +0100

    cleaned dependencies loader - now nicely support unknow platform, and import dependencies in the right order

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# disable warnings for nltk and dependency overwrite
+
 try:
     import commands
     arch = "_%s"%commands.getoutput('uname -m')
@@ -7,25 +10,18 @@ except:
     arch = ""
 import sys
 
-sys.path = [ 
-   "lib/python-%s.%s_%s%s" % (sys.version_info[0],sys.version_info[1], sys.platform, arch), # C MODULES
-   "lib/python-%s.%s" % (sys.version_info[0],sys.version_info[1]), # PYTHON MODULES
-    'src'] + sys.path # PROJECT MODULES, AND DEFAULT SYSTEM MODULES 
-print sys.path
+sys.path += [ # try to get libraries installed in the system first
+    'src',
+   "lib/python-%s.%s_%s%s" % (
+          sys.version_info[0],sys.version_info[1], 
+          sys.platform, 
+          arch),
+   "lib/universal" ] # in last resort, we use the packaged pure-python libraries (but they are slow)
 
-try:
-    import nltk
-    import yaml
-    import jsonpickle
-    import pickle
-    import os
-    import sys
-except Exception, exc:
-    print "Dependency not found:", exc
-# import nltk data
+
+import warnings 
+warnings.filterwarnings("ignore") 
 import nltk
 nltk.data.path = ['shared/nltk_data']
 
-# disable warnings
-import warnings 
-warnings.filterwarnings("ignore") 
+
