20f2b5dbbac51cac8908b87b14b13e2566a0d35f
elishowk <elishowk@nonutc.fr>
2010-02-05 13:46:45 +0100

    updated tests

--- tests/cooccurrences.py
+++ tests/cooccurrences.py
@@ -18,28 +18,36 @@ class CoocTestCase(unittest.TestCase):
             storage='tinabsddb://fetopen.test.bsddb')
         self.filtertag = ngram.PosTagFilter()
         self.filterContent = ngram.Filter()
+        self.filterstop = stopwords.StopWordFilter(
+            'file:///home/elishowk/TINA/Datas/100126-fetopen-stopwords-from-david.csv'
+        )
 
     def testAllFiltersCooc(self):
         """analyse a corpus, applying all filters available"""
         def corpusAnalyse( self, id ):
-            cooc = cooccurrences.MapReduce(self.tinasoft.storage, corpus=id, filter=[self.filtertag, self.filterContent])
+            cooc = cooccurrences.MapReduce(self.tinasoft.storage, corpus=id, filter=[self.filtertag, self.filterContent, self.filterstop])
             self.tinasoft.logger.debug( "Test : Starting walkCorpus" )
             cooc.walkCorpus()
             self.tinasoft.logger.debug( "Test : Starting writeMatrix" )
             cooc.writeMatrix()
-        corpusAnalyse( self, '1' )
+        #corpusAnalyse( self, '1' )
         corpusAnalyse( self, '2' )
         corpusAnalyse( self, '3' )
         corpusAnalyse( self, '4' )
         corpusAnalyse( self, '5' )
-        corpusAnalyse( self, '6' )
-        corpusAnalyse( self, '7' )
-        corpusAnalyse( self, '8' )
-        self.tinasoft.logger.debug( "Test : Starting select('Cooc')" )
-        generator = self.tinasoft.storage.select('Cooc')
+        #corpusAnalyse( self, '6' )
+        #corpusAnalyse( self, '7' )
+        #corpusAnalyse( self, '8' )
+
+
+    def testCountCooc(self):
+        return
+        self.tinasoft.logger.debug( "Test : Starting selectCorpusCooc('1')" )
+        corpusId = '1'
+        count = 0
+        generator = self.tinasoft.storage.selectCorpusCooc(corpusId)
         try:
             record = generator.next()
-            count = 0
             while record:
                 #self.assertEqual( isinstance(record[1], dict), True)
                 #self.tinasoft.logger.debug( record )
@@ -48,14 +56,7 @@ class CoocTestCase(unittest.TestCase):
         except StopIteration, si:
             #self.tinasoft.logger.debug( "===End of printing the \
             #    20 first Cooc entries in the db" )
-            self.tinasoft.logger.debug( "Total Cooc rows in database = "+ str(count))
-
-    def testCorpusCooc(self):
-        return
-        corpus = '1'
-        selectgenerator = self.tinasoft.storage.select('NGram')
-        cooc = cooccurrences.Simple(selectgenerator, self.tinasoft.storage, corpus)
-        cooc.walkCorpus()
+            self.tinasoft.logger.debug( "Corpus " +corpusId+ " has Cooc rows = "+ str(count))
 
 if __name__ == '__main__':
     unittest.main()
