b2524165a744ee66d479e207ea398ff8d0643939
elishowk <elishowk@nonutc.fr>
2010-02-23 17:21:07 +0100

    updated tests/filter.py and andded a db cursor pickle

--- tests/filter.py
+++ tests/filter.py
@@ -8,34 +8,34 @@ __date__ ="$Jan, 21 2010"
 import unittest
 import os
 import random
-
-from tinasoft import TinaApp
 from tinasoft.pytextminer import *
+import pickle
 
 class TestsTestCase(unittest.TestCase):
     def setUp(self):
-        self.tinasoft = TinaApp(configFile='config.yaml',\
-            storage='tinabsddb://fetopen.test.bsddb')
+        self.cursor = pickle.load( open('tests/data/ngram-dbcursor.pickle', 'r') )
 
     def testAll(self):
-        selectgenerator = self.tinasoft.storage.select('NGram')
         filtertag = ngram.PosTagFilter()
         filtercontent = ngram.Filter()
         try:
             count=0
-            countFilter=0
-            while count<50:
-                filterrecord = selectgenerator.next()
+            countFilterContent =0
+            countFilterTag = 0
+            for record in self.cursor:
                 count+=1
-                res = filtertag.all( filterrecord[1] ) and\
-                    filtercontent.all( filterrecord[1] )
-                if res is False:
-                    countFilter += 1
+                if filtertag.test( record[1] ) is False:
+                    countFilterTag += 1
+                if filtercontent.test( record[1] ) is False:
+                    countFilterContent += 1
             raise StopIteration()
         except StopIteration, si:
-            print "all() filters eliminated = ", countFilter
+            print "content filters eliminated = ", countFilterContent
+            print "POS tag filter eliminated = ", countFilterTag
             print "Total NGrams = ", count
-            return
+            self.assertEqual( countFilterContent, 4 )
+            self.assertEqual( countFilterTag, 8 )
+            self.assertEqual( count, 50 )
 
 if __name__ == '__main__':
     unittest.main()
