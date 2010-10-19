976ce319df18a64adb0f661927a582fbf1275224
elishowk <elishowk@nonutc.fr>
2010-10-19 14:55:15 +0200

    updated freeze_mac

--- freeze_mac.py
+++ freeze_mac.py
@@ -12,27 +12,28 @@ from setuptools import setup
 
 APP = ['Tinasoft.py']
 DATA_FILES = [
-    ('',glob(r'desktop_config_unix.yaml')),
-    ('',glob(r'LICENSE')),
-    ('',glob(r'README')),
-    ('source_files',glob(join('source_files','*.csv'))),
-    (join('shared','gexf'), glob(join('shared','gexf','gexf.template'))),
-    (join('shared','stopwords'), glob(join('shared','stopwords','*'))),
-    (join('shared','nltk_data','corpora','brown'), glob(join('shared','nltk_data','corpora','brown','*'))),
-    (join('shared','nltk_data','corpora','conll2000'), glob(join('shared','nltk_data','corpora','conll2000','*'))),
-    (join('shared','nltk_data','tokenizers','punkt'), glob(join('shared','nltk_data','tokenizers','punkt','*')))
+    #('',glob(r'desktop_config_unix.yaml')),
+    #('',glob(r'LICENSE')),
+    #('',glob(r'README')),
+    #('source_files',glob(join('source_files','*.csv'))),
+    #(join('shared','gexf'), glob(join('shared','gexf','gexf.template'))),
+    #(join('shared','stopwords'), glob(join('shared','stopwords','*'))),
+    #(join('shared','nltk_data','corpora','brown'), glob(join('shared','nltk_data','corpora','brown','*'))),
+    #(join('shared','nltk_data','corpora','conll2000'), glob(join('shared','nltk_data','corpora','conll2000','*'))),
+    #(join('shared','nltk_data','tokenizers','punkt'), glob(join('shared','nltk_data','tokenizers','punkt','*')))
 ]
 
 OPTIONS = {
-'argv_emulation': True,
-'argv_inject': "desktop_config_unix.yaml",
-'packages': ['numpy'],
-'includes': ['tinasoft.data.gexf','tinasoft.data.medline','tinasoft.data.tinasqlite','tinasoft.data.tinacsv','tinasoft.data.whitelist','tinasoft.data.coocmatrix','tinasoft.data.basecsv','traceback','zope.interface','twisted','nltk','numpy','jsonpickle','yaml','tenjin','simplejson'],
-'excludes': ['bsddb3','tinasoft.data.tinabsddb']
-'resources': DATA_FILES,
-'plist': {
-    'LSEnvironment': {'NLTK_DATA':'shared/nltk_data'}
-},
+	'argv_emulation': True,
+	'argv_inject': "config_unix.yaml",
+	#'packages': ['nltk','numpy','twisted','twisted.web','twisted.internet','encodings','zope.interface'],
+	'includes': ['tinasoft.data.gexf','tinasoft.data.medline','tinasoft.data.tinasqlite','tinasoft.data.tinacsv','tinasoft.data.whitelist','tinasoft.data.coocmatrix','tinasoft.data.basecsv','traceback','zope.interface','twisted','nltk','numpy','jsonpickle','yaml','tenjin','simplejson','twisted.web','twisted.internet','encodings'],
+	'excludes': ['_gtkagg', '_tkagg', 'curses', 'email', 'tcl',
+            'Tkconstants', 'Tkinter','tinasoft.data.tinabsddb','bsddb3'],
+	#'resources': DATA_FILES,
+	'plist': {
+	    'LSEnvironment': {'NLTK_DATA':'shared/nltk_data'}
+	},
 }
 setup(
     app=APP,
