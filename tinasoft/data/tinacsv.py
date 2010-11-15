b2481ad0b0163d05d080bd61e658086190e5fb17
elishowk <elishowk@nonutc.fr>
2010-11-15 15:35:43 +0100

    simplified tinacsv field names

--- tinasoft/data/tinacsv.py
+++ tinasoft/data/tinacsv.py
@@ -19,8 +19,8 @@ class Importer (basecsv.Importer):
         titleField: 'doc_titl',
         contentField: 'doc_abst',
         authorField: 'doc_acrnm',
-        corpusNumberField: 'corp_num',
-        docNumberField: 'doc_num',
+        corpusField: 'corp_num',
+        docField: 'doc_num',
         index1Field: 'index_1',
         index2Field: 'index_2',
         dateField: 'date',
@@ -34,31 +34,29 @@ class Importer (basecsv.Importer):
         updates corpus and corpora edges
         """
         self.line = 0
-        #try:
         for doc in self:
-            #doc = self.csv.next()
             if doc is None: continue
             self.line += 1
             tmpfields = dict(self.fields)
             # decoding & parsing TRY
             try:
-                corpusNumber = doc[self.fields['corpusNumberField']]
-                del tmpfields['corpusNumberField']
+                corpusID = doc[self.fields['corpusField']]
+                del tmpfields['corpusField']
+                #if not isinstance( corpusID, unicode ) or not isinstance( corpusID, str ):
+                #    raise Exception("%s is not in string format"%corpusID)
             except Exception, exc:
                 _logger.error( "tinacsv error : corpus id missing at line %d"%self.line )
                 _logger.error( exc )
                 continue
             # TODO check if corpus already exists
-            newdoc = self._parse_document( doc, tmpfields, corpusNumber )
+            newdoc = self._parse_document( doc, tmpfields, corpusID )
             if newdoc is None: continue
             # if corpus DOES NOT already exist
-            if corpusNumber not in self.corpusDict:
+            if corpusID not in self.corpusDict:
                 # creates a new corpus and adds it to the global dict
-                self.corpusDict[ corpusNumber ] = corpus.Corpus( corpusNumber )
+                self.corpusDict[ corpusID ] = corpus.Corpus( corpusID )
             # sends the document and the corpus id
-            yield newdoc, corpusNumber
-        #except StopIteration, sit:
-        #    return
+            yield newdoc, corpusID
 
     def _parse_document( self, doc, tmpfields, corpusNum ):
         """
@@ -69,14 +67,16 @@ class Importer (basecsv.Importer):
         # parsing TRY
         try:
             # get required fields
-            docNum = doc[tmpfields['docNumberField']]
+            docID = doc[tmpfields['docField']]
             content = doc[tmpfields['contentField']]
             title  = doc[tmpfields['titleField']]
-            del tmpfields['docNumberField']
+            del tmpfields['docField']
             del tmpfields['contentField']
             del tmpfields['titleField']
+            #if not isinstance( docID, unicode ) or not isinstance( docID, str ):
+            #    raise Exception("%s is not in string format"%docID)
         except Exception, exc:
-            _logger.error("error parsing document %s at line %d : skipping"%(str(docNum),self.line))
+            _logger.error("error parsing document %s at line %d : skipping"%(docID,self.line))
             _logger.error(exc)
             return None
         # parsing optional fields loop and TRY
@@ -84,7 +84,8 @@ class Importer (basecsv.Importer):
             try:
                 docArgs[ field ] = doc[ field ]
             except Exception, exc:
-                _logger.warning("missing a document's field %s at line %s"%(field,str(self.line)))
+                _logger.warning("missing a document's field %s at line %d"%(field,self.line))
+
         if 'dateField' in docArgs:
             datestamp = docArgs[ 'dateField' ]
         else:
@@ -92,7 +93,7 @@ class Importer (basecsv.Importer):
         # new document
         newdoc = document.Document(
             content,
-            docNum,
+            docID,
             title,
             datestamp=datestamp,
             **docArgs
