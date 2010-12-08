8bbb3c14350089f55b4b1b3e410922d210dacb72
elishowk <elishowk@nonutc.fr>
2010-12-08 17:41:03 +0100

    tests graph data set

--- tests/tinacsv_test_3_data.py
+++ tests/tinacsv_test_3_data.py
@@ -15,3 +15,115 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "elishowk@nonutc.fr"
+
+def get_tinacsv_test_3_data(ngram_prox="cooccurrences", doc_prox="sharedNGrams"):
+    return {
+        'nodes' : {
+            "NGram::3743759f962f379be74fbd207d5c2e8ed68bb56751763772f7370bc13b76bb7e": {
+                'weight': 3,
+                'label': "TINA EC ICT FP7 CA"
+            },
+            "NGram::a7a9ed6c4c233c9fc17c8ff33d4f490df8750503939d726ca856d109a2c1bd98": {
+                'weight': 2,
+                'label': "visualisation"
+            },
+            "NGram::818c1d3377c1b53a31b756404dd07fcf642fbb59c30ab27c1f92cb1e746570dc": {
+                'weight': 2,
+                'label': "analysis and visualisation"
+            },
+            "NGram::0d712987367dce537111041aa572fc3f368c50b99b2ed06271840f15ae88af3e": {
+                'weight': 2,
+                'label': "scientific research"
+            },
+            "NGram::a9f6eb8ed71f6e1d6e8121cf05e6d7d3362382010d973681a9e2351e3ab2c958": {
+                'weight': 1,
+                'label': "interactive assessement"
+            },
+            "Document::60": {
+                'weight': 1,
+                'label': "DUPLICATE"
+            },
+            "Document::59": {
+                'weight': 1,
+                'label': "TINA"
+            },
+            "Document::55": {
+                'weight': 1,
+                'label': "UNIQUE"
+            },
+        },
+        'edges': {
+            "NGram::3743759f962f379be74fbd207d5c2e8ed68bb56751763772f7370bc13b76bb7e" : {
+                "NGram::a7a9ed6c4c233c9fc17c8ff33d4f490df8750503939d726ca856d109a2c1bd98": 2,
+                "NGram::818c1d3377c1b53a31b756404dd07fcf642fbb59c30ab27c1f92cb1e746570dc": 2,
+                "NGram::0d712987367dce537111041aa572fc3f368c50b99b2ed06271840f15ae88af3e": 2,
+                "NGram::a9f6eb8ed71f6e1d6e8121cf05e6d7d3362382010d973681a9e2351e3ab2c958": 1,
+                "Document::60": 1,
+                "Document::59": 1,
+                "Document::55": 1,
+            },
+            "NGram::a7a9ed6c4c233c9fc17c8ff33d4f490df8750503939d726ca856d109a2c1bd98" : {
+                "NGram::3743759f962f379be74fbd207d5c2e8ed68bb56751763772f7370bc13b76bb7e": 2,
+                "NGram::818c1d3377c1b53a31b756404dd07fcf642fbb59c30ab27c1f92cb1e746570dc": 2,
+                "NGram::0d712987367dce537111041aa572fc3f368c50b99b2ed06271840f15ae88af3e": 2,
+                "NGram::a9f6eb8ed71f6e1d6e8121cf05e6d7d3362382010d973681a9e2351e3ab2c958": 1,
+                "Document::60": 1,
+                "Document::59": 2,
+                #"Document::55": 0,
+            },
+            "NGram::818c1d3377c1b53a31b756404dd07fcf642fbb59c30ab27c1f92cb1e746570dc": {
+                "NGram::3743759f962f379be74fbd207d5c2e8ed68bb56751763772f7370bc13b76bb7e": 2,
+                "NGram::a7a9ed6c4c233c9fc17c8ff33d4f490df8750503939d726ca856d109a2c1bd98": 2,
+                "NGram::0d712987367dce537111041aa572fc3f368c50b99b2ed06271840f15ae88af3e": 2,
+                "NGram::a9f6eb8ed71f6e1d6e8121cf05e6d7d3362382010d973681a9e2351e3ab2c958": 1,
+                "Document::60": 1,
+                "Document::59": 1,
+                #"Document::55": 0,
+            },
+            "NGram::0d712987367dce537111041aa572fc3f368c50b99b2ed06271840f15ae88af3e": {
+                "NGram::3743759f962f379be74fbd207d5c2e8ed68bb56751763772f7370bc13b76bb7e": 2,
+                "NGram::a7a9ed6c4c233c9fc17c8ff33d4f490df8750503939d726ca856d109a2c1bd98": 2,
+                "NGram::818c1d3377c1b53a31b756404dd07fcf642fbb59c30ab27c1f92cb1e746570dc": 2,
+                "NGram::a9f6eb8ed71f6e1d6e8121cf05e6d7d3362382010d973681a9e2351e3ab2c958": 1,
+                "Document::60": 1,
+                "Document::59": 1,
+                #"Document::55": 0,
+            },
+            "NGram::a9f6eb8ed71f6e1d6e8121cf05e6d7d3362382010d973681a9e2351e3ab2c958": {
+                "NGram::3743759f962f379be74fbd207d5c2e8ed68bb56751763772f7370bc13b76bb7e": 1,
+                "NGram::a7a9ed6c4c233c9fc17c8ff33d4f490df8750503939d726ca856d109a2c1bd98": 1,
+                "NGram::818c1d3377c1b53a31b756404dd07fcf642fbb59c30ab27c1f92cb1e746570dc": 1,
+                "NGram::0d712987367dce537111041aa572fc3f368c50b99b2ed06271840f15ae88af3e": 1,
+                "Document::60": 1,
+                #"Document::59": 0,
+                #"Document::55": 0,
+            },
+            "Document::60": {
+                "NGram::3743759f962f379be74fbd207d5c2e8ed68bb56751763772f7370bc13b76bb7e": 1,
+                "NGram::a7a9ed6c4c233c9fc17c8ff33d4f490df8750503939d726ca856d109a2c1bd98": 1,
+                "NGram::818c1d3377c1b53a31b756404dd07fcf642fbb59c30ab27c1f92cb1e746570dc": 1,
+                "NGram::0d712987367dce537111041aa572fc3f368c50b99b2ed06271840f15ae88af3e": 1,
+                "NGram::a9f6eb8ed71f6e1d6e8121cf05e6d7d3362382010d973681a9e2351e3ab2c958": 1,
+                "Document::59": 4,
+                "Document::55": 1,
+            },
+            "Document::59": {
+                "NGram::3743759f962f379be74fbd207d5c2e8ed68bb56751763772f7370bc13b76bb7e": 1,
+                "NGram::a7a9ed6c4c233c9fc17c8ff33d4f490df8750503939d726ca856d109a2c1bd98": 2,
+                "NGram::818c1d3377c1b53a31b756404dd07fcf642fbb59c30ab27c1f92cb1e746570dc": 1,
+                "NGram::0d712987367dce537111041aa572fc3f368c50b99b2ed06271840f15ae88af3e": 1,
+                "NGram::a9f6eb8ed71f6e1d6e8121cf05e6d7d3362382010d973681a9e2351e3ab2c958": 0,
+                "Document::60": 4,
+                "Document::55": 1,
+            },
+            "Document::55": {
+                "NGram::3743759f962f379be74fbd207d5c2e8ed68bb56751763772f7370bc13b76bb7e": 1,
+                #"NGram::a7a9ed6c4c233c9fc17c8ff33d4f490df8750503939d726ca856d109a2c1bd98": 0,
+                #"NGram::818c1d3377c1b53a31b756404dd07fcf642fbb59c30ab27c1f92cb1e746570dc": 0,
+                #"NGram::0d712987367dce537111041aa572fc3f368c50b99b2ed06271840f15ae88af3e": 0,
+                #"NGram::a9f6eb8ed71f6e1d6e8121cf05e6d7d3362382010d973681a9e2351e3ab2c958": 0,
+                "Document::60": 1,
+                "Document::59": 1,
+            },
+        }      
+    }
