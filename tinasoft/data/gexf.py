d40a1c737660cb098d7a89abdbc0c23c52c90980
elishowk <elishowk@nonutc.fr>
2010-12-10 15:56:08 +0100

    cleaned print

--- tinasoft/data/gexf.py
+++ tinasoft/data/gexf.py
@@ -105,7 +105,6 @@ class Exporter(Handler):
                 if self.preprocess is True:
                     self._update_preprocess_db(nodeid, row, category)
                 if self.generate is True:
-                    print nodeid, row
                     self._update_graph_db(nodeid, row, category)
                     # adds node and its weight to the template var
                     self.graph['nodes'][category][nodeid] = matrix.get(nodeid, nodeid)
